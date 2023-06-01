0cfc7fa578cac5d46384cd1efae7d5bff48cdafe
Hazuki-295 <luosheng233@outlook.com>
2023-06-01 16:51:20 +0800

    Update visualization.py

--- tutorials/open3d/visualization.py
+++ tutorials/open3d/visualization.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 import open3d as o3d
 
 
@@ -7,10 +9,19 @@ def visualization(filename):
     pcd = o3d.io.read_point_cloud(filename)
 
     # This function takes a list of geometry objects, and renders them together.
-    window_name = "Visualization — " + os.path.basename(filename)
-    o3d.visualization.draw_geometries([pcd], window_name, width=1000, height=800, left=200, top=150)
+    o3d.visualization.draw_geometries([pcd], window_name="Visualization — {}".format(os.path.basename(filename)),
+                                      width=1000, height=800, left=200, top=150)
 
 
 if __name__ == "__main__":
-    input_filename = "data/input/bunny.ply"
-    visualization(input_filename)
+    filenames = []
+
+    if len(sys.argv) > 1:
+        filenames = sys.argv[1:]
+    else:
+        print("No input filenames passed.\n")
+        filenames.append("data/input/bunny.ply")
+
+    for index, input_filename in enumerate(filenames):
+        print(f"Visualize file [{index + 1}]: {input_filename}")
+        visualization(input_filename)
