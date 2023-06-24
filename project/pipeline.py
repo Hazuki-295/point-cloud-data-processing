4ef314f7b3d8228c3ac4a7c8c74d4deb93034159
Hazuki-295 <luosheng233@outlook.com>
2023-06-25 01:44:44 +0800

    Update pipeline.py

--- project/pipeline.py
+++ project/pipeline.py
@@ -1,6 +1,8 @@
 import os
 import sys
+from collections import Counter
 
+import numpy as np
 import open3d as o3d
 
 
@@ -18,6 +20,41 @@ def pipeline(filename):
 
     # Outlier removal
     filtered_pcd, _ = down_sampled_pcd.remove_statistical_outliers(nb_neighbors=20, std_ratio=2.0)
+
+    # Mask by intensity to acquire low intensity points
+    intensity_threshold = 50
+    mask = np.where(filtered_pcd.point.intensity.numpy().flatten() <= intensity_threshold, True, False)
+
+    # Extract low intensity points, then perform DBSCAN on them
+    low_intensity_pcd = filtered_pcd.select_by_mask(mask)
+    labels = low_intensity_pcd.cluster_dbscan(eps=0.10, min_points=5)
+    labels = labels.numpy()
+
+    n_clusters = labels.max() + 1
+    print(f"DBSCAN clustering return {n_clusters} clusters.\n")
+
+    # Clusters will be labeled in a way that cluster with the most points is labeled 1
+    if n_clusters >= 2:
+        counter = Counter(labels)
+
+        # Remove noise that be labeled -1
+        del counter[-1]
+
+        # Sort by count
+        sorted_items = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+        rank = [item[0] for item in sorted_items]
+
+        # Convert mask to index
+        index_of_mask = [i for i, boolean_val in enumerate(mask) if boolean_val]
+
+        # Add cluster attribute to the input point cloud
+        cluster = np.zeros(len(mask)).astype(np.int32)
+        for i, val in enumerate(labels):
+            if val != -1:
+                cluster[index_of_mask[i]] = rank.index(val) + 1
+        filtered_pcd.point.cluster = np.reshape(cluster, (len(cluster), 1))
+    else:
+        print("Warning: DBSCAN clustering return less than 2 clusters.")
 
 
 if __name__ == "__main__":
